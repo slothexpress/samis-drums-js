916e2419695474576f989daef19b3d5fa64c3ffb
slothexpress <hellosami@protonmail.com>
2022-12-26 15:46:02 +0100

    Add sounds to all buttons

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 
 var drumsArray = document.querySelectorAll(".drum")
-var audio = new Audio("sounds/tom-1.mp3");
 
 drumsArray.forEach(element => {
-    element.addEventListener("click", () => audio.play());
+
+    var drum = element.classList[0];
+    switch (drum) {
+        case "w":
+            var audioW = new Audio("sounds/tom1.mp3");
+            element.addEventListener("click", () => audioW.play());
+            break;
+        case "a":
+            var audioA = new Audio("sounds/tom2.mp3");
+            element.addEventListener("click", () => audioA.play());
+            break;
+        case "s":
+            audioS = new Audio("sounds/tom3.mp3");
+            element.addEventListener("click", () => audioS.play());
+            break;
+        case "d":
+            audioD = new Audio("sounds/tom4.mp3");
+            element.addEventListener("click", () => audioD.play());
+            break;
+        case "j":
+            audioJ = new Audio("sounds/snare.mp3");
+            element.addEventListener("click", () => audioJ.play());
+            break;
+        case "k":
+            audioK = new Audio("sounds/kick.mp3");
+            element.addEventListener("click", () => audioK.play());
+            break;
+        case "l":
+            audioL = new Audio("sounds/kick.mp3");
+            element.addEventListener("click", () => audioL.play());
+            break;
+        default:
+            break;
+    };
 });
 
 
