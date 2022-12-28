ee65fd39a2bce26e5e8851b79fe4b4c95baf39b3
slothexpress <hellosami@protonmail.com>
2022-12-28 09:48:14 +0100

    Refactor into function

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
+addSoundsToButtons();
 
-var drumsArray = document.querySelectorAll(".drum")
+function addSoundsToButtons() {
+    var drumsArray = document.querySelectorAll(".drum")
 
-drumsArray.forEach(element => {
-    element.addEventListener("click", function () {
+    drumsArray.forEach(element => {
+        element.addEventListener("click", function () {
 
-        var drum = element.classList[0];
-        switch (drum) {
-            case "w":
-                var audioW = new Audio("sounds/tom1.mp3");
-                audioW.play();
-                break;
-            case "a":
-                var audioA = new Audio("sounds/tom2.mp3");
-                audioA.play();
-                break;
-            case "s":
-                audioS = new Audio("sounds/tom3.mp3");
-                audioS.play();
-                break;
-            case "d":
-                audioD = new Audio("sounds/tom4.mp3");
-                audioD.play();
-                break;
-            case "j":
-                audioJ = new Audio("sounds/snare.mp3");
-                audioJ.play();
-                break;
-            case "k":
-                audioK = new Audio("sounds/kick.mp3");
-                audioK.play();
-                break;
-            case "l":
-                audioL = new Audio("sounds/crash.mp3");
-                audioL.play();
-                break;
-            default:
-                console.log("Switch/Default: " + element.innerHTML);
-                break;
-        };
+            var drum = element.classList[0];
+            switch (drum) {
+                case "w":
+                    var audioW = new Audio("sounds/tom1.mp3");
+                    audioW.play();
+                    break;
+                case "a":
+                    var audioA = new Audio("sounds/tom2.mp3");
+                    audioA.play();
+                    break;
+                case "s":
+                    audioS = new Audio("sounds/tom3.mp3");
+                    audioS.play();
+                    break;
+                case "d":
+                    audioD = new Audio("sounds/tom4.mp3");
+                    audioD.play();
+                    break;
+                case "j":
+                    audioJ = new Audio("sounds/snare.mp3");
+                    audioJ.play();
+                    break;
+                case "k":
+                    audioK = new Audio("sounds/kick.mp3");
+                    audioK.play();
+                    break;
+                case "l":
+                    audioL = new Audio("sounds/crash.mp3");
+                    audioL.play();
+                    break;
+                default:
+                    console.log("Switch/Default: " + element.innerHTML);
+                    break;
+            };
 
-    })
+        })
 
-});
+    });
+}
+
