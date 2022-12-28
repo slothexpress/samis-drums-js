94d7693bad92f526ca95ab3d9b39f67f6b3625cc
slothexpress <hellosami@protonmail.com>
2022-12-28 09:52:04 +0100

    Add eventlisteners for keyboard press

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 addSoundsToButtons();
+addSoundsToKeyboard();
 
 function addSoundsToButtons() {
     var drumsArray = document.querySelectorAll(".drum")
@@ -46,3 +47,43 @@ function addSoundsToButtons() {
     });
 }
 
+function addSoundsToKeyboard() {
+
+    document.addEventListener("keypress", event => {
+        var key = event.key;
+        switch (key) {
+            case "w":
+                var audioW = new Audio("sounds/tom1.mp3");
+                audioW.play();
+                break;
+            case "a":
+                var audioA = new Audio("sounds/tom2.mp3");
+                audioA.play();
+                break;
+            case "s":
+                audioS = new Audio("sounds/tom3.mp3");
+                audioS.play();
+                break;
+            case "d":
+                audioD = new Audio("sounds/tom4.mp3");
+                audioD.play();
+                break;
+            case "j":
+                audioJ = new Audio("sounds/snare.mp3");
+                audioJ.play();
+                break;
+            case "k":
+                audioK = new Audio("sounds/kick.mp3");
+                audioK.play();
+                break;
+            case "l":
+                audioL = new Audio("sounds/crash.mp3");
+                audioL.play();
+                break;
+            default:
+                console.log("Switch/Default: " + element.innerHTML);
+                break;
+        };
+    })
+
+}
